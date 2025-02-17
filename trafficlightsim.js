f863aae05758093b0744b58b5c2f33c5b6a2be88
Godwin Homadzi <godwinhomadzi820@gmail.com>
2025-02-17 13:28:47 -0800

    the is a traffic light sim

---
+++ trafficlightsim.js
@@ -0,0 +1,16 @@
+let trafficLight = "pink" ;
+
+if (trafficLight === "red"){
+    console.log("stop! and wait until the light turns green");
+}
+
+else if (trafficLight === "yellow"){
+    console.log("prepare to stop or wait");
+}
+
+else if (trafficLight === "green"){
+    console.log("you can go now");
+}
+else{
+console.log("invalid traffic light color")
+}
